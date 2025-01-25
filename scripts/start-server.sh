e1d21a91d8a3f19e01469b4b302809c5a76c3b51
jaepil <jaepill94@gmail.com>
2025-01-25 15:17:40 +0900

    [feat] CICD 구축 - appspec.yml 작성 - 서버 쉘스크립트 작성 Resolves: #16

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 echo "--------------- 서버 배포 시작 -----------------"
-docker stop for-work-server || true
-docker rm for-work-server || true
-docker rmi 730335533510.dkr.ecr.ap-northeast-2.amazonaws.com/for-work-server:latest || true
-docker pull 730335533510.dkr.ecr.ap-northeast-2.amazonaws.com/for-work-server:latest
-docker run -d --name for-work-server -p 8080:8080 730335533510.dkr.ecr.ap-northeast-2.amazonaws.com/for-work-server:latest
+docker stop search-info-server || true
+docker rm search-info-server || true
+docker rmi 730335533510.dkr.ecr.ap-northeast-2.amazonaws.com/search-info-server:latest || true
+docker pull 730335533510.dkr.ecr.ap-northeast-2.amazonaws.com/search-info-server:latest
+docker run -d --name search-info-server -p 8080:8080 730335533510.dkr.ecr.ap-northeast-2.amazonaws.com/search-info-server:latest
 echo "--------------- 서버 배포 끝 -----------------"
